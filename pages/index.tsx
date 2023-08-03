e6262efb2778e3272bca2963c351fbb0b92c494f
Vaibhav Sharma <vaibhav.sharma@aize.io>
2023-08-03 23:14:11 +0200

    attempt number 2 to add analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { createContext, useContext, useState } from "react";
 import { SearchRequest } from "./api/search";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
+import { Analytics } from "@vercel/analytics/react";
 
 interface RequestContextType {
   request: SearchRequest;
@@ -34,26 +35,31 @@ const Home: React.FC = () => {
   const [request, setRequest] = useState<SearchRequest>(emptySearchRequest);
 
   return (
-    <RequestContext.Provider value={{ request, setRequest }}>
-      <QueryClientProvider client={queryClient}>
-        <div className="finder">
-          <header className="bg-gray-800 py-4">
-            <div className="container mx-auto px-1">
-              <h1 className="text-white text-3xl font-bold">Finder</h1>
-            </div>
-          </header>
-          <main className="flex flex-col px-24 py-4">
-            <div className="py-4 pb-12 px-4">
-              <SearchBar queryHandler={setRequest} query={request}></SearchBar>
-            </div>
+    <>
+      <RequestContext.Provider value={{ request, setRequest }}>
+        <QueryClientProvider client={queryClient}>
+          <div className="finder">
+            <header className="bg-gray-800 py-4">
+              <div className="container mx-auto px-1">
+                <h1 className="text-white text-3xl font-bold">Finder</h1>
+              </div>
+            </header>
+            <main className="flex flex-col px-24 py-4">
+              <div className="py-4 pb-12 px-4">
+                <SearchBar
+                  queryHandler={setRequest}
+                  query={request}></SearchBar>
+              </div>
 
-            <div className="py-4 px-4 bg-gray-50">
-              <Result request={request}></Result>
-            </div>
-          </main>
-        </div>
-      </QueryClientProvider>
-    </RequestContext.Provider>
+              <div className="py-4 px-4 bg-gray-50">
+                <Result request={request}></Result>
+              </div>
+            </main>
+          </div>
+        </QueryClientProvider>
+      </RequestContext.Provider>
+      <Analytics />
+    </>
   );
 };
 
